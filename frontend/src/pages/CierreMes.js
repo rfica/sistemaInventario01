74731530496e345a0c38509d8890fca21669e1c7
richard.fica <richard.fica@gmail.com>
2025-06-14 02:51:54 +0000

    Mejora en el look Cierre de mes

--- frontend/src/pages/CierreMes.js
+++ frontend/src/pages/CierreMes.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import * as periodService from '../services/periodService';
 
+import '../styles/CierreMes.css';
 const CierreMes = () => {
   const [selectedMonth, setSelectedMonth] = useState('');
   const [selectedYear, setSelectedYear] = useState('');
