38e5d42f62480c21c08020237595f1d88096a5d5
richard.fica <richard.fica@gmail.com>
2025-06-15 05:27:45 +0000

    crea usuario rol2

--- backend/scripts/createTestUser.js
+++ backend/scripts/createTestUser.js
@@ -4,23 +4,30 @@ const { poolPromise } = require('../config/db'); // O './config/database' si ese
 
 async function createTestUser() {
 
-    // Ejecutar la consulta INSERT para crear el usuario
-    const result = await pool.request()
-      .input('username', 'cerrador') // Parámetro para el nombre de usuario
+ try {
+ // Obtener una conexión del pool
+ const pool = await poolPromise;
+
+ const password = '123'; // Contraseña temporal para el usuario de prueba
+ const hashedPassword = await bcrypt.hash(password, 10);
+
+ // Ejecutar la consulta INSERT para crear el usuario
+ const result = await pool.request()
+ .input('username', 'cerrador') // Parámetro para el nombre de usuario
  .input('email', 'cerrador@inventario.local') // Parámetro para el correo
-      .input('passwordHash', hashedPassword) // Parámetro para el hash de la contraseña
-      .input('roleId', 2) // Parámetro para el RoleId (2 para Cerrador de Mes)
-      .query('INSERT INTO [User] (Username, Email, PasswordHash, RoleId) VALUES (@username, @email, @passwordHash, @roleId)');
+ .input('passwordHash', hashedPassword) // Parámetro para el hash de la contraseña
+ .input('roleId', 2) // Parámetro para el RoleId (2 para Cerrador de Mes)
+ .query('INSERT INTO [User] (Username, Email, PasswordHash, RoleId) VALUES (@username, @email, @passwordHash, @roleId)');
 
-    console.log('Usuario de prueba creado exitosamente.');
-    console.log('Username:', 'cerrador');
-    // console.log('Password:', password); // Considera omitir la contraseña por seguridad
-    console.log('Hash:', hashedPassword); // Puedes mostrar el hash generado
+ console.log('Usuario de prueba creado exitosamente.');
+ console.log('Username:', 'cerrador');
+ // console.log('Password:', password); // Considera omitir la contraseña por seguridad
+ console.log('Hash:', hashedPassword); // Puedes mostrar el hash generado
 
-    process.exit(0); // Salir con éxito
-  } catch (error) {
-    console.error('Error:', error); // Mostrar cualquier error
-    process.exit(1); // Salir con error
+ process.exit(0); // Salir con éxito
+ } catch (error) {
+ console.error('Error al crear el usuario:', error); // Mostrar cualquier error
+ process.exit(1); // Salir con error
   }
 }
 
