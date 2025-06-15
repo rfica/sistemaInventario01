647ca857a14f8751a8fd50c27ab92a906bd7c292
richard.fica <richard.fica@gmail.com>
2025-06-15 03:00:45 +0000

    modifica para poder cerrar el mes

--- backend/routes/periodRoutes.js
+++ backend/routes/periodRoutes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const periodController = require('../controllers/periodController'); // Asegúrate de que la ruta sea correcta
 const authMiddleware = require('../middleware/auth'); // Asumiendo que auth.js es tu middleware de autenticación
 
-console.log('✅ periodRoutes loaded');
-
 // Middleware para logging de solicitud en el router de períodos
 router.use((req, res, next) => {
- console.log('➡️ Period Router received request:', req.method, req.url);
- next();
+    console.log('➡️ Period Router received request:', req.method, req.url);
+    next();
 });
 // Ruta para obtener los períodos cerrados
 router.get('/closed', periodController.getClosedPeriods);
@@ -16,4 +14,6 @@ router.get('/closed', periodController.getClosedPeriods);
 // Ruta para cerrar un período
 router.post('/closePeriod', periodController.closePeriod);
 
+// Todas las rutas que requieran autenticación deben usar el middleware aquí
+router.post('/closePeriod', authMiddleware, periodController.closePeriod);
 module.exports = router;
